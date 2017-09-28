9fca1d47b4a1e4bc159421282bc0a5ffbee3a4b7
Jakob Urnes <urnes038@umn.edu>
2017-09-28 18:30:28 -0500

    gave randy some cheeseburgers

---
+++ P1/p1-code/p1-code/cmd.c
@@ -0,0 +1,36 @@
+#include <sctdio.h>
+#include <stdlib.h>
+#include <string.h>
+// cmd.c
+
+cmd_t *cmd_new(char *argv[]){
+        cmd_t *cmd = malloc(sizeof (cmd_t));
+        cmd->name = strdup(argv[0]);
+        //for (int i = 0;
+        //cmd->argv = strdup(argv);
+        cmd->pid = NULL;
+        cmd->out_pipe = [-1,-1];
+        cmd->finished = 0;
+        cmd->status = -1;
+        cmd->str_status = snprintf("INIT");
+        cmd->output = NULL;
+        cmd->output_size = -1;
+}
+
+void cmd_free(cmd_t *cmd){
+        //deallocate strings in argv
+        if (cmd->output != NULL){
+                free(cmd->output);
+        }
+        free(cmd);
+}
+void cmd_info(cmd_t *cmd);
+void cmd_start(cmd_t *cmd){
+        cmd->out_pipe = pipe(int pipes[2]);
+        cmd->str_status = snprintf("RUN");
+        cmd->pid = fork();
+}
+void cmd_fetch_output(cmd_t *cmd);
+void cmd_print_output(cmd_t *cmd);
+void cmd_update_state(cmd_t *cmd, int nohang);
+
