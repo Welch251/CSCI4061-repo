b5519a533c9f016cc64d029176a5a1e643666f26
Evan Welch <welch251@umn.edu>
2017-10-01 20:59:21 -0500

    continued editing read_all, fetch output, and print output

--- P1/p1-code/cmd.c
+++ P1/p1-code/cmd.c
@@ -9,7 +9,6 @@ cmd_t *cmd_new(char *argv[]){
         for(int i=0; i = ARG_MAX; i++){
           cmd->argv[i] = strdup(argv[i]);
         }
-        cmd->argv = strdup(argv);
         cmd->argv[ARG_MAX+1] = NULL;
         cmd->pid = NULL;
         cmd->out_pipe = NULL;
@@ -23,7 +22,7 @@ cmd_t *cmd_new(char *argv[]){
 void cmd_free(cmd_t *cmd){
         //deallocate strings in argv
         if (cmd->output != NULL){
-                free(cmd->output);
+          free(cmd->output);
         }
         free(cmd);
 }
@@ -35,7 +34,7 @@ void cmd_start(cmd_t *cmd){
 	if (cmd->pid == 0){
 		dup2(PWRITE,cmd->out_pipe[PWRITE]);
 		close(cmd->out_pipe[PREAD]);
-		//execvp()
+		execvp(cmd->name,cmd->argv);
 	}
 	else{
 		dup2(PREAD,cmd->out_pipe[PREAD]);
@@ -43,13 +42,33 @@ void cmd_start(cmd_t *cmd){
 	}
 }
 char *read_all(int fd, int *nread){
-  char *buf = malloc(BUFSIZE);
-  
+  char *buf = (char *) malloc(BUFSIZE);
+  int need_space = read(fd,buf,sizeof buf);
+  while(need_space > 0){
+    buf = (char *) realloc(buf, 2 * sizeof buf);
+    need_space = read(fd,buf,sizeof buf);
+  }
+  nread = sizeof buf;
+  buf[(sizeof buf) + 1] = '\0';  // may go outside of buffer range?!?!?
+  return buf;
 }
 void cmd_fetch_output(cmd_t *cmd){
-
+  if(!cmd->finished){
+    printf("%s[#%d] not finished yet", cmd->name, cmd->pid);
+  }
+  else{
+    cmd->output = read_all(cmd->out_pipe[PREAD], cmd->output_size);
+    close(cmd->out_pipe[PREAD]);
+  }
 }
-void cmd_print_output(cmd_t *cmd);
+void cmd_print_output(cmd_t *cmd){
+  if(cmd->output = NULL){
+    printf("%s[#%d] has no output yet", cmd->name, cmd->pid);
+  }
+  else{
+    write(STDOUT_FILENO, cmd->output, cmd->output_size);
+  }
+}
 void cmd_update_state(cmd_t *cmd, int nohang){
 	if (cmd->finished != 1){
 		waitpid(cmd->pid,&status,nohang);
@@ -57,7 +76,7 @@ void cmd_update_state(cmd_t *cmd, int nohang){
 			cmd->finished = 1;
 			cmd->status = WEXITSTATUS(status);
 			cmd->str_status = sprintf("EXIT(%d)",cmd->status);
-			//cmd_fetch_output(cmd);
+			cmd_fetch_output(cmd);
 			printf("@!!! %s[#%d]: %s",cmd->name,cmd->pid, cmd->str_status);
 
 		}
