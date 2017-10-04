78fe3054159bef2c51a260adfcb85ae49db09c13
Jakob Urnes <urnes038@umn.edu>
2017-10-04 17:42:40 -0500

    painted Bubbles' shed

--- P1/p1-code/cmdctl.c
+++ P1/p1-code/cmdctl.c
@@ -1,3 +1,5 @@
+#include "commando.h"
+
 void cmdctl_add(cmdctl_t *ctl, cmd_t *cmd){
 	ctl->cmd[ctl->size] = cmd;
 	ctl->size = ctl->size + 1;
@@ -10,20 +12,21 @@ void cmdctl_print(cmdctl_t *ctl){
 	cmd_t *current;
 	for (int i = 0; i<ctl->size; i++){
 		current = ctl->cmd[i];
-		printf("%-4d #%-8d %4d %10s %4d %s", i, current->pid, current->status,current->str_status, current->output_size, current->argv);
+		printf("%-4d #%-8d %4d %10s %4d %s", i, current->pid, current->status,
+			current->str_status, current->output_size, current->name);
 	}
 }
 // Print all cmd elements in the given ctl structure.  The format of
 // the table is
 //
 // JOB  #PID      STAT   STR_STAT OUTB COMMAND
-// 0    #17434       0    EXIT(0) 2239 ls -l -a -F 
-// 1    #17435       0    EXIT(0) 3936 gcc --help 
-// 2    #17436      -1        RUN   -1 sleep 2 
-// 3    #17437       0    EXIT(0)  921 cat Makefile 
-// 
+// 0    #17434       0    EXIT(0) 2239 ls -l -a -F
+// 1    #17435       0    EXIT(0) 3936 gcc --help
+// 2    #17436      -1        RUN   -1 sleep 2
+// 3    #17437       0    EXIT(0)  921 cat Makefile
+//
 // Widths of the fields and justification are as follows
-// 
+//
 // JOB  #PID      STAT   STR_STAT OUTB COMMAND
 // 1234  12345678 1234 1234567890 1234 Remaining
 // left  left    right      right rigt left
@@ -34,18 +37,18 @@ void cmdctl_print(cmdctl_t *ctl){
 
 void cmdctl_update_state(cmdctl_t *ctl, int block){
 	cmd_t *current;
-	for (int k = 0; k<clt->size; k++){
-		current = ctl->cmd[i];
+	for (int k = 0; k < ctl->size; k++){
+		current = ctl->cmd[k];
 		cmd_update_state(current,block);
 	}
 }
 // Update each cmd in ctl by calling cmd_update_state() which is also
-// passed the block argument (either NOBLOCK or DOBLOCK) 
+// passed the block argument (either NOBLOCK or DOBLOCK)
 
 void cmdctl_freeall(cmdctl_t *ctl){
 	cmd_t *current;
-	for (int k = 0; k<clt->size; k++){
-		current = ctl->cmd[i];
+	for (int k = 0; k < ctl->size; k++){
+		current = ctl->cmd[k];
 		cmd_free(current);
 	}
 }
