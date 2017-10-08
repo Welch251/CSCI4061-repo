44777820f9978dd9d5cf0af2a18f28d5e6c16bf4
Jakob Urnes <urnes038@umn.edu>
2017-10-08 18:02:05 -0500

    added comments to cmd.c and cmdctl.c

--- P1/p1-code/cmdctl.c
+++ P1/p1-code/cmdctl.c
@@ -8,54 +8,33 @@ void cmdctl_add(cmdctl_t *ctl, cmd_t *cmd){
 // size field.
 
 void cmdctl_print(cmdctl_t *ctl){
-	printf("JOB  #PID      STAT   STR_STAT OUTB COMMAND\n");
+	printf("JOB  #PID      STAT   STR_STAT OUTB COMMAND\n"); 			//initial listing of each component presented in a cmd
 	cmd_t *current;
-	for (int i = 0; i<ctl->size; i++){
+	for (int i = 0; i<ctl->size; i++){						//retrieve and list information for each cmd
 		current = ctl->cmd[i];
-		printf("%-4d #%-8d %4d %10s %4d ", i, current->pid, current->status,
+		printf("%-4d #%-8d %4d %10s %4d ", i, current->pid, current->status,	//print the cmd's elements in the correct format
 			current->str_status, current->output_size);
 			int j = 0;
-		while(current->argv[j] != NULL){
+		while(current->argv[j] != NULL){					//retrieve the rest of the cmd's arguments
 			printf("%s ", current->argv[j]);
 			j++;
 		}
 		printf("\n");
 	}
 }
-// Print all cmd elements in the given ctl structure.  The format of
-// the table is
-//
-// JOB  #PID      STAT   STR_STAT OUTB COMMAND
-// 0    #17434       0    EXIT(0) 2239 ls -l -a -F
-// 1    #17435       0    EXIT(0) 3936 gcc --help
-// 2    #17436      -1        RUN   -1 sleep 2
-// 3    #17437       0    EXIT(0)  921 cat Makefile
-//
-// Widths of the fields and justification are as follows
-//
-// JOB  #PID      STAT   STR_STAT OUTB COMMAND
-// 1234  12345678 1234 1234567890 1234 Remaining
-// left  left    right      right rigt left
-// int   int       int     string  int string
-//
-// The final field should be the contents of cmd->argv[] with a space
-// between each element of the array.
 
 void cmdctl_update_state(cmdctl_t *ctl, int block){
 	cmd_t *current;
-	for (int k = 0; k < ctl->size; k++){
+	for (int k = 0; k < ctl->size; k++){						//iterate through each cmd in the ctl structure
 		current = ctl->cmd[k];
-		cmd_update_state(current,block);
+		cmd_update_state(current,block);					//update the state of the selected cmd
 	}
 }
-// Update each cmd in ctl by calling cmd_update_state() which is also
-// passed the block argument (either NOBLOCK or DOBLOCK)
 
 void cmdctl_freeall(cmdctl_t *ctl){
 	cmd_t *current;
-	for (int k = 0; k < ctl->size; k++){
+	for (int k = 0; k < ctl->size; k++){						//iterate through each cmd in the ctl structure
 		current = ctl->cmd[k];
-		cmd_free(current);
+		cmd_free(current);							//free the memory of the selected cmd
 	}
 }
-// Call cmd_free() on all of the constituent cmd_t's.
