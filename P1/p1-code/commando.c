cd8a66a101ac0c7f1d414180f697e61d8f36dd55
Evan Welch <welch251@umn.edu>
2017-10-08 03:04:29 -0500

    actually printing output.  Fixed null ptr issue with ntok

--- P1/p1-code/commando.c
+++ P1/p1-code/commando.c
@@ -7,14 +7,13 @@ int main(int argc, char *argv[]){
   cmdctl_t *ctl = malloc(sizeof (cmdctl_t));
   ctl->size = 0;
   int echo_status = 0;
-  if(strcmp(argv[1],"--echo") || getenv("COMMANDO_ECHO") != NULL){
+  if(strcmp(argv[0],"--echo") || getenv("COMMANDO_ECHO") != NULL){
     echo_status = 1;
   }
   while(1){
     printf("@> ");
     char str[MAX_LINE];
-    char *success;
-    success = fgets(str, MAX_LINE, stdin);
+    char *success = fgets(str, MAX_LINE, stdin);
     if(success == NULL){
       printf("End of input");
       break;
@@ -22,10 +21,11 @@ int main(int argc, char *argv[]){
     if(echo_status){
       printf("%s\n", str);
     }
-    int *ntok = NULL;
+    int ntok;
     char *tokens[ARG_MAX];
-    parse_into_tokens(str, tokens, ntok);
-    if(strcmp(tokens[0], "help") != 0){
+    parse_into_tokens(str, tokens, &ntok);
+    if(ntok == 0){}
+    else if(strcmp(tokens[0], "help") == 0){
       printf("\nCOMMANDO COMMANDS\n");
       printf("help               : show this message\n");
       printf("exit               : exit the program\n");
@@ -37,28 +37,28 @@ int main(int argc, char *argv[]){
       printf("wait-all           : wait for all jobs to finish\n");
       printf("command arg1 ...   : non-built-in is run as a job\n");
     }
-    else if(strcmp(tokens[0], "exit") != 0){
+    else if(strcmp(tokens[0], "exit") == 0){
       break;
     }
-    else if(strcmp(tokens[0], "list") != 0){
+    else if(strcmp(tokens[0], "list") == 0){
       cmdctl_print(ctl);
     }
-    else if(strcmp(tokens[0], "pause nanos secs") != 0){
+    else if(strcmp(tokens[0], "pause nanos secs") == 0){
       pause_for(atoi(tokens[1]),atoi(tokens[2]));
     }
-    else if(strcmp(tokens[0], "output-for int") != 0){
+    else if(strcmp(tokens[0], "output-for int") == 0){
       cmd_fetch_output(ctl->cmd[atoi(tokens[1])]);
     }
-    else if(strcmp(tokens[0], "output-all") != 0){
+    else if(strcmp(tokens[0], "output-all") == 0){
       for (int i = 0; i<ctl->size;i++){
           cmd_fetch_output(ctl->cmd[atoi(tokens[1])]);
       }
     }
-    else if(strcmp(tokens[0], "wait-for int") != 0){
+    else if(strcmp(tokens[0], "wait-for int") == 0){
        cmd_t *process = ctl->cmd[atoi(tokens[1])];
        cmd_update_state(process,DOBLOCK);
     }
-    else if(strcmp(tokens[0], "wait-all") != 0){
+    else if(strcmp(tokens[0], "wait-all") == 0){
        wait(NULL);
     }
     else{
