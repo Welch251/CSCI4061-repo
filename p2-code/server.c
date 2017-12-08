09fb8e1611d611cb45752e66ff7fd05ce79861da
Evan Welch <welch251@umn.edu>
2017-12-08 11:09:29 -0600

    added method names for server.c

---
+++ p2-code/server.c
@@ -0,0 +1,50 @@
+#include "blather.h"
+
+client_t *server_get_client(server_t *server, int idx){
+
+}
+void server_start(server_t *server, char *server_name, int perms){
+
+}
+void server_shutdown(server_t *server){
+
+}
+int server_add_client(server_t *server, join_t *join){
+
+}
+int server_remove_client(server_t *server, int idx){
+
+}
+int server_broadcast(server_t *server, mesg_t *mesg){
+
+}
+void server_check_sources(server_t *server){
+
+}
+int server_join_ready(server_t *server){
+
+}
+int server_handle_join(server_t *server){
+
+}
+int server_client_ready(server_t *server, int idx){
+
+}
+int server_handle_client(server_t *server, int idx){
+
+}
+void server_tick(server_t *server){
+
+}
+void server_ping_clients(server_t *server){
+
+}
+void server_remove_disconnected(server_t *server, int disconnect_secs){
+
+}
+void server_write_who(server_t *server){
+
+}
+void server_log_message(server_t *server, mesg_t *mesg){
+
+}
