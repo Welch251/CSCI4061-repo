afd7e39afe0fae00d0ca5260791be741f229ec2e
Evan Welch <welch251@umn.edu>
2017-12-08 20:32:20 -0600

    started writing methods for server.c

--- p2-code/server.c
+++ p2-code/server.c
@@ -4,13 +4,44 @@ client_t *server_get_client(server_t *server, int idx){
   return server->client[idx];
 }
 void server_start(server_t *server, char *server_name, int perms){
-
+  snprintf(server->server_name, MAXPATH, "%s", server_name);
+  remove(server->server_name);
+  ret = mkfifo(server->server_name, perms);
+  if(ret < 0){
+    printf("fifo can't be made\n");
+    exit(0);
+  }
+  server->join_fd = open(server->server_name, perms);
+  if(server->join_fd < 0){
+    printf("fifo can't be opened");
+    exit(0);
+  }
+  server->join_ready = 1;
+  server->n_clients = 0;
+  sever->client = NULL;
 }
 void server_shutdown(server_t *server){
-
+  close(server->join_fd);
+  remove(server->server_name);
+  mesg_t *msg;
+  msg->kind = BL_SHUTDOWN;
+  msg->name = NULL;
+  msg->body = NULL;
+  while(server->n_clients > 0){
+    ret = write(server->client[server->n_clients-1]->to_client_fd,msg,sizeof(mesg_t));
+    if(ret < 0){
+      printf("write to client failed");
+      exit(0);
+    }
+    server->client[server->n_clients-1] = NULL;
+    server->n_clients--;
+  }
 }
 int server_add_client(server_t *server, join_t *join){
-
+  client_t *client;
+  snprintf(client->name, MAXPATH, join->name);
+  snprintf(client->name, MAXPATH, join->name);
+  snprintf(client->name, MAXPATH, join->name);
 }
 int server_remove_client(server_t *server, int idx){
 
