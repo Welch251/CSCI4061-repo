c305f51edc6ae468b12e5e8beb77aa84314081c0
Jakob Urnes <urnes038@umn.edu>
2017-12-08 23:57:24 -0600

    updated bl-client

--- p2-code/bl-client.c
+++ p2-code/bl-client.c
@@ -1,5 +1,59 @@
 #include "blather.h"
 
+simpio_t simpio_actual;
+simpio_t *simpio = &simpio_actual;
+
+client_t client_actual;
+client_t *client = &client_actual;
+
+pthread_t user_thread;          // thread managing user input
+pthread_t server_thread;	// thread managing server input
+
+// Worker thread to manage user input
+void *user_feed(void *arg){
+  mesg_t *mesg_to_s;
+  snprintf(mesg_to_s->name, MAXNAME, "%s", user_name);
+  while(!simpio->end_of_input){
+    simpio_reset(simpio);
+    iprintf(simpio, "");                                          // print prompt
+    while(!simpio->line_ready && !simpio->end_of_input){          // read until line is complete
+      simpio_get_char(simpio);
+    }
+    if(simpio->line_ready){
+      mesg_t *mesg_to_s;
+      snprintf(mesg_to_s->body, MAXLINE, "%s", simpio->buf);
+      mesg_to_s->kind = BL_MESG;
+      write(ts_fd, mesg_to_s, sizeof(mesg_t));
+    }
+  }
+
+  pthread_cancel(server_thread); // kill the background thread
+  return NULL;
+}
+
+// Worker thread to listen to the info from the server.
+void *server_feed(void *arg){
+  while (1){
+    mesg_t *mesg_to_c;
+    read(tc_fd, mesg_to_c, sizeof(mesg_t));
+    char terminal_mesg[MAXLINE];
+    if (mesg_to_c->kind == BL_MESG){
+      snprintf(terminal_mesg, MAXLINE, "[%s]: %s", mesg_to_c->name, mesg_to_c->body);
+    }
+    else if (mesg_to_c->kind == BL_JOINED){
+      snprintf(terminal_mesg, MAXLINE, "-- %s JOINED --", mesg_to_c->name);
+    }
+    else if (mesg_to_c->kind == BL_DEPARTED){
+      snprintf(terminal_mesg, MAXLINE, "-- %s DEPARTED --", mesg_to_c->name);
+    }
+    else if (mesg_to_c->kind == BL_SHUTDOWN){
+      snprintf(terminal_mesg, MAXLINE, "!!! sunnyvale is shutting down !!!");
+    }
+    iprintf(simpio, terminal_mesg); 
+  }
+  return NULL;
+}
+
 
 int main(int argc, char *argv[]){
   char * serv_fname = argv[1];			//Retrieve name info from input
@@ -39,37 +93,19 @@ int main(int argc, char *argv[]){
 
   write(serv_fd, join, sizeof(join_t));		//Write join request to server's FIFO
 
-  if (user){					//user thread loop
-    while (!eoi){
-      mesg_t *mesg_to_s;
-      snprintf(mesg_to_s->name, MAXNAME, "%s", user_name); 
-      fgets(mesg_to_s->body, MAXLINE, STDIN_FILENO);
-      mesg_to_s->kind = BL_MESG;  
-      write(ts_fd, mesg_to_s, sizeof(mesg_t));
-    }
-    
-  }
-  else{						//server thread loop
-    while (!shutdown){
-      mesg_t *mesg_to_c;
-      read(tc_fd, mesg_to_c, sizeof(mesg_t)); 
-      char terminal_mesg[MAXLINE];
-      if (mesg_to_c->kind == BL_MESG){
-        snprintf(terminal_mesg, MAXLINE, "[%s]: %s", mesg_to_c->name, mesg_to_c->body);
-      }
-      else if (mesg_to_c->kind == BL_JOINED){
-        snprintf(terminal_mesg, MAXLINE, "-- %s JOINED --", mesg_to_c->name);
+  char prompt[MAXNAME];
+  snprintf(prompt, MAXNAME, "%s>> ",user_name); // create a prompt string
+  simpio_set_prompt(simpio, prompt);         // set the prompt
+  simpio_reset(simpio);                      // initialize io
+  simpio_noncanonical_terminal_mode();       // set the terminal into a compatible mode
 
-      }
-      else if (mesg_to_c->kind == BL_DEPARTED){
-	snprintf(terminal_mesg, MAXLINE, "-- %s DEPARTED --", mesg_to_c->name);
 
-      }
-      else if (mesg_to_c->kind == BL_SHUTDOWN){
-	snprintf(terminal_mesg, MAXLINE, "!!! sunnyvale is shutting down !!!");
-      }
-      iprintf(
-    }
+  pthread_create(&user_thread, NULL, user_feed, NULL);
+  pthread_create(&server_thread, NULL, server_feed, NULL);
+  pthread_join(user_thread, NULL);
+  pthread_join(server_thread, NULL);
 
-  }
+  simpio_reset_terminal_mode();
+  printf("\n");                 // newline just to make returning to the terminal prettier
+
 }
