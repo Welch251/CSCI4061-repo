04beff066bc4114f02842c08d2accc25f0f5c93d
Evan Welch <welch251@umn.edu>
2017-12-10 00:40:40 -0600

    created main for bl-server.c

---
+++ p2-code/bl-server.c
@@ -0,0 +1,26 @@
+#include "blather.h"
+
+int signalled = 0;
+
+void sig_handler(int signo) {
+  signalled = 1;
+}
+
+int main(int argc, char *argv[]){
+  char * serv_fname = argv[1];
+  struct server_t *server;
+  server_start(server, serv_fname, DEFAULT_PERMS);
+  signal(SIGTERM, sig_handler);
+  signal(SIGINT, sig_handler);
+  while (!signalled){
+    server_check_sources(server);
+    server_handle_join(server);
+    for (int i=0; i < server->n_clients; i++){
+      if (server_client_ready(server,i)){
+        server_handle_client(server,i);
+      }
+    }
+  }
+  server_shutdown(server);
+  return 0;
+}
