5d897401efbd210f3a18c4cd42dabfb45cc7b94d
Evan Welch <welch251@umn.edu>
2017-12-14 13:36:20 -0600

    added comments to files

--- p2-code/bl-client.c
+++ p2-code/bl-client.c
@@ -19,71 +19,68 @@ typedef enum { false, true } bool;
 // Worker thread to manage user input
 void *user_feed(void *arg){
   struct arg_struct *args = (struct arg_struct *)arg;
-  int ts_fd = open(args->arg1, O_WRONLY);				  //Open the to-server FIFO
-  char * user_name = args->arg2;
+  int ts_fd = open(args->arg1, O_WRONLY);				  // Open the to-server FIFO
+  char * user_name = args->arg2;					  // Retrieve the username
   mesg_t msg;
   mesg_t *mesg_to_s = &msg;
-  memset(mesg_to_s, 0, sizeof(mesg_t));
-  snprintf(mesg_to_s->name, MAXNAME, "%s", user_name);
+  memset(mesg_to_s, 0, sizeof(mesg_t));					  // Initialize message struct bytes to default values of 0
+  snprintf(mesg_to_s->name, MAXNAME, "%s", user_name);			  // Write user name to message struct
   while(!simpio->end_of_input) {
-    //Critical
     simpio_reset(simpio);
-    iprintf(simpio, "");                         // print prompt
-    while(!simpio->line_ready && !simpio->end_of_input){
+    iprintf(simpio, "");                			          // print prompt
+    while(!simpio->line_ready && !simpio->end_of_input){		  // Take in user input, one char at a time
       simpio_get_char(simpio);
     }
     if(simpio->line_ready){
-      snprintf(mesg_to_s->body, MAXLINE, "%s", simpio->buf);
-      mesg_to_s->kind = BL_MESG;
-      write(ts_fd, mesg_to_s, sizeof(mesg_t));
+      snprintf(mesg_to_s->body, MAXLINE, "%s", simpio->buf); 		  // Once input for a line is complete, print the line to the message struct
+      mesg_to_s->kind = BL_MESG;					  // Set message kind to appropriate value BL_MESG
+      write(ts_fd, mesg_to_s, sizeof(mesg_t));				  // Send message to to-server FIFO
     }
-    //Region
   }
-  pthread_cancel(server_thread); // kill the background thread
-  mesg_to_s->kind = BL_DEPARTED;
-  write(ts_fd, mesg_to_s, sizeof(mesg_t));
-  close(ts_fd);
+  // Thread shutdown
+  pthread_cancel(server_thread); 					  // kill the background thread
+  mesg_to_s->kind = BL_DEPARTED;					  // Set message kind to appropriate value BL_DEPARTED
+  write(ts_fd, mesg_to_s, sizeof(mesg_t));				  // Send DEPART message to to-server FIFO
+  close(ts_fd);								  // Close the to-server FIFO
   return NULL;
 }
 
 // Worker thread to listen to the info from the server.
 void *server_feed(void *arg){
-  char *user_fname_tc = (char *)arg;
-  int tc_fd = open(user_fname_tc, O_RDONLY);				//Open the to-client FIFO
+  char *user_fname_tc = (char *)arg;					// Retrieve the username
+  int tc_fd = open(user_fname_tc, O_RDONLY);				// Open the to-client FIFO
   while (1){
-    //Critical
     mesg_t mesg;
     mesg_t *mesg_to_c = &mesg;
-    memset(mesg_to_c, 0, sizeof(mesg_t));
-    read(tc_fd, mesg_to_c, sizeof(mesg_t));
+    memset(mesg_to_c, 0, sizeof(mesg_t));				// Initialize message struct bytes to default values of 0
+    read(tc_fd, mesg_to_c, sizeof(mesg_t));				// Read message from to-server client into message struct
     char terminal_mesg[MAXLINE];
-    if (mesg_to_c->kind == BL_MESG){
-      snprintf(terminal_mesg, MAXLINE, "[%s] : %s \n", mesg_to_c->name, mesg_to_c->body);
+    if (mesg_to_c->kind == BL_MESG){					// Message kind is MESG
+      snprintf(terminal_mesg, MAXLINE, "[%s] : %s \n", mesg_to_c->name, mesg_to_c->body); // Display the given user's message
     }
-    else if (mesg_to_c->kind == BL_JOINED){
-      snprintf(terminal_mesg, MAXLINE, "-- %s JOINED -- \n", mesg_to_c->name);
+    else if (mesg_to_c->kind == BL_JOINED){				// Message kind is JOINED
+      snprintf(terminal_mesg, MAXLINE, "-- %s JOINED -- \n", mesg_to_c->name);  // Display notification that the given user has joined
     }
-    else if (mesg_to_c->kind == BL_DEPARTED){
-      snprintf(terminal_mesg, MAXLINE, "-- %s DEPARTED -- \n", mesg_to_c->name);
+    else if (mesg_to_c->kind == BL_DEPARTED){				// Message kind is DEPARTED
+      snprintf(terminal_mesg, MAXLINE, "-- %s DEPARTED -- \n", mesg_to_c->name); // Display notification that the given user has left
     }
-    else if (mesg_to_c->kind == BL_SHUTDOWN){
-      snprintf(terminal_mesg, MAXLINE, "!!! server is shutting down !!!\n");
+    else if (mesg_to_c->kind == BL_SHUTDOWN){				// Message kind is SHUTDOWN
+      snprintf(terminal_mesg, MAXLINE, "!!! server is shutting down !!!\n");  // Display notification that the server is shutting down
       iprintf(simpio, terminal_mesg);
       break;
     }
-    iprintf(simpio, terminal_mesg);
-    //Region
+    iprintf(simpio, terminal_mesg);					// Ensure that the input prompt is ahead of any message 
   }
-  close(tc_fd);
-  pthread_cancel(user_thread); // kill the background thread
+  close(tc_fd);								// Close the to-client FIFO
+  pthread_cancel(user_thread);						// kill the background thread
   return NULL;
 }
 
 
 int main(int argc, char *argv[]){
 
-  if(argc != 3){
-    printf("usage: %s <program> <int>\n",argv[0]);
+  if(argc != 3){				//Check if correct number of arguments has been entered,
+    printf("usage: %s <program> <int>\n",argv[0]); // if not, notify user and exit
     exit(0);
   }
 
@@ -101,7 +98,8 @@ int main(int argc, char *argv[]){
 
   int serv_fd;					//File descriptor for server FIFO
 
-  strcpy(user_fname_tc,"");			//Add name info to char arrays
+  strcpy(user_fname_tc,"");			//Properly set up names of FIFOs in relation to entered username
+
   strcpy(user_fname_ts,"");
   strcat(user_fname_tc,user_name);
   strcat(user_fname_tc,"_to_cl \0");
@@ -122,9 +120,9 @@ int main(int argc, char *argv[]){
   snprintf(join->to_client_fname, MAXPATH, "%s", user_fname_tc); //Send to-client name info to join struct
   snprintf(join->to_server_fname, MAXPATH, "%s", user_fname_ts); //Send to-server name info to join struct
 
-  serv_fd = open(serv_fname, O_RDWR); 		//Open the server's FIFO
+  serv_fd = open(serv_fname, O_RDWR); 		// Open the server's join FIFO, set as READ and WRITE
 
-  struct arg_struct args;
+  struct arg_struct args;			// Initialize struct to contain the two pieces of information needed by the user thread
   args.arg1 = user_fname_ts;
   args.arg2 = user_name;
 
@@ -136,11 +134,12 @@ int main(int argc, char *argv[]){
   simpio_reset(simpio);                      // initialize io
   simpio_noncanonical_terminal_mode();       // set the terminal into a compatible mode
 
-  pthread_create(&user_thread, NULL, user_feed, (void *) &args);
-  pthread_create(&server_thread, NULL, server_feed, (void *) user_fname_tc);
-  pthread_join(user_thread, NULL);
-  pthread_join(server_thread, NULL);
-  close(serv_fd);
+  pthread_create(&user_thread, NULL, user_feed, (void *) &args);		// Create thread for user input
+  pthread_create(&server_thread, NULL, server_feed, (void *) user_fname_tc);    // Create thread for server output
+  pthread_join(user_thread, NULL);						// Regroup user thread following cancellation
+  pthread_join(server_thread, NULL);						// Regroup server thread following cancellation
+  close(serv_fd);								// Close the server's join FIFO
   simpio_reset_terminal_mode();
   printf("\n");                 // newline just to make returning to the terminal prettier
 }
+
--- p2-code/bl-server.c
+++ p2-code/bl-server.c
@@ -3,33 +3,34 @@
 int signalled = 0;
 
 void sig_handler(int signo) {
-  signalled = 1;
+  signalled = 1;						// Signifies that loop should stop
 }
 
 int main(int argc, char *argv[]){
   if(argc != 2){
-    printf("usage: %s <server name> \n",argv[0]);
-    exit(0);
+    printf("usage: %s <server name> \n",argv[0]);		// Check if the correct number of arguments was entered,
+    exit(0);							// if not, notify user and exit 
   }
 
-  char * serv_fname = argv[1];
+  char * serv_fname = argv[1];					// Sets the server's name equal to the first argument
   server_t server;
-  server_t *serv = &server;
+  server_t *serv = &server;					// Initialize the server struct
   memset(serv, 0, sizeof(server_t));
   server_start(serv, serv_fname, DEFAULT_PERMS);
-  signal(SIGTERM, sig_handler);
-  signal(SIGINT, sig_handler);
-  while (!signalled){
-    server_check_sources(serv);
-    if(server_join_ready(serv)){
+  signal(SIGTERM, sig_handler);					// Replace default SIGTERM disposition with the sig_handler call
+  signal(SIGINT, sig_handler);					// Replace default SIGINT disposition with the sig_handler call
+  while (!signalled){						// Continue through loop as long as not signalled, allows for graceful shutdown
+    server_check_sources(serv);					// Check if there are any new join requests
+    if(server_join_ready(serv)){				// If so, handle the join request and add the client to the server
       server_handle_join(serv);
     }
-    for (int i=0; i < serv->n_clients; i++){
-      if(server_client_ready(serv, i)){
+    for (int i=0; i < serv->n_clients; i++){			// Go through the list of registered clients, check if they have 
+      if(server_client_ready(serv, i)){                         // new data that is ready to be processed
         server_handle_client(serv,i);
       }
     }
   }
-  server_shutdown(serv);
+  server_shutdown(serv);                                       // Call shutdown sequence for server
   return 0;
 }
+
